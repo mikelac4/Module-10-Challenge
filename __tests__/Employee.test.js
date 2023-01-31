b6785494d548a96c136ca86670c07bec30927f82
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-01-31 14:06:19 -0500

    Created employee test file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,48 @@
-//
+const Employee = require("../lib/Employee")
+
+test("Creates new employee", () => {
+    const newEmp = new Employee();
+    expect(typeof(newEmp)).toBe("object");
+});
+
+test("Sets the name", () => {
+    const name = "Joe";
+    const newEmp = new Employee(name);
+    expect(newEmp.name).toBe(name);
+});
+
+test("Sets id", () => {
+    const id = 6;
+    const newEmp = new Employee("Joe", id);
+    expect(newEmp.id).toBe(id);
+});
+
+test("Sets an email", () => {
+    const email = "email@email.com";
+    const newEmp = new Employee("Joe", 6, email);
+    expect(newEmp.email).toBe(email);
+});
+
+test("Gets name from getName()", () => {
+    const name = "Joe";
+    const newEmp = new Employee(name);
+    expect(newEmp.getName()).toBe(name);
+});
+
+test("Gets id from getId()", () => {
+    const id = 6;
+    const newEmp = new Employee("Joe", id);
+    expect(newEmp.getId()).toBe(id); 
+});
+
+test("Gets email from getEmail()", () => {
+    const email = "email@email.com";
+    const newEmp = new Employee("Joe", 6, email);
+    expect(newEmp.getEmail()).toBe(email);
+});
+
+test("Gets role", () => {
+    const role = "Employee";
+    const newEmp = new Employee("Joe", 6, "email@email.com");
+    expect(newEmp.getRole()).toBe(role);
+});
