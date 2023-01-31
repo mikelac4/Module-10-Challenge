e03c76a748a0579e588e6ec9d5ef076652e63a6c
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-01-31 14:35:50 -0500

    Created engineer test file

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,20 @@
-//
+const Engineer = require("../lib/Engineer")
+
+test("Sets GitHub", () => {
+    const git = "Joe123";
+    const eng = new Engineer("Joe", 6, "email@email.com", git);
+    expect(eng.github).toBe(git);
+});
+
+test("Gets GitHub from getGithub()", () => {
+    const git = "Joe123";
+    const eng = new Engineer("Joe", 6, "email@email.com", git);
+    expect(eng.getGithub()).toBe(git);
+
+});
+
+test("Get role", () => {
+    const role = "Engineer";
+    const eng = new Engineer("Joe", 6, "email@email.com", "Joe123");
+    expect(eng.getRole()).toBe(role);
+});
