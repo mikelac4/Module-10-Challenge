a40f86d2969de0709f7cda116e97922617f215f6
mikelac4 <112447725+mikelac4@users.noreply.github.com>
2023-01-31 15:27:01 -0500

    Created manager test file

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,21 @@
-//
+const { default: test } = require("node:test");
+const Manager = require("../lib/Manager");
+
+test("Sets office number", () => {
+    const num = 24;
+    const man = new Manager("Joe", 6, "email@email.com", num);
+    expect(man.officeNumber).toBe(num);
+});
+
+test("Gets office number from getOfficeNumber()", () => {
+    const num = 24;
+    const man = new Manager("Joe", 6, "email@email.com", num);
+    expect(man.getOfficeNumber()).toBe(num);
+
+});
+
+test("Gets role", () => {
+    const role = "Manager";
+    const man = new Manager("Joe", 6, "email@email.com", 24);
+    expect(man.getRole()).toBe(role);
+});
